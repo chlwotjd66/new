9ffeaa551c972e969ef92037e49ff6cd2008c0eb
chlwotjd66 <choi_jasung@naver.com>
2023-05-27 23:45:48 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -2,17 +2,21 @@
 
 const id = document.querySelector("#id"),
      pw = document.querySelector("#pw"),
-    loginBtn = document.querySelector("#button");
+     nikname = document.querySelector("#nikname"),
+     confirmPw = document.querySelector("#confirm-pw"),
+    registerBtn = document.querySelector("#button");
 
-loginBtn.addEventListener("click", login);
+registerBtn.addEventListener("click", register);
 
-function login(){
+function register(){
     const req = {
         id : id.value,
         pw : pw.value,
+        nikname : nikname.value,
+        confirmPw : confirmPw.value
     };
-
-    fetch("/login", { 
+    console.log(req);
+    fetch("/register", { 
      method: "POST",
      headers: {
             "Content-Type" : "application/json",
@@ -22,13 +26,13 @@ function login(){
     }).then((res) => res.json())
     .then((res) => {
      if (res.success){
-        location.bref = "/";
+        location.bref = "/login";
          } else {
             alert(res.msg);
          }
     })
     .catch((err) => {
-        console.error("로그인 중 에러 발생");
+        console.error("회원가입 중 에러 발생");
     });
 }
     
