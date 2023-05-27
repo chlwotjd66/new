edc524cc3dc27e89a25ed9703d61926e3b18c106
chlwotjd66 <choi_jasung@naver.com>
2023-05-27 17:36:35 +0900

    DOM 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,18 @@
 "use strict";
 
-console.log("hello"); 
-console.log("bye"); 
+const id = document.querySelector("#id"),
+     pw = document.querySelector("#pw"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id : id.value,
+        pw : pw.value,
+    };
+    console.log(req)
+}
+    
+//DOM -> Document Object Model이 js에서 html을 가져와 동작하게 함
+
