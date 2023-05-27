fc11586f3a7785ee95cb9953b7e13c44c826dc4b
chlwotjd66 <choi_jasung@naver.com>
2023-05-27 21:20:10 +0900

    user 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -16,7 +16,17 @@ class UserStorage{
             return newUsers;
         }, {}); 
         return newUsers;
+    }
 
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users); //=> [id, pw, name]
+        const userInfo = Object.keys(users).reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+        return userInfo;
     }
 }
 
